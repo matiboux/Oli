25233b74da53e0d00ea26cf023f9a4c369ec2523
Matiboux <matiboux@gmail.com>
2017-03-29 19:05:07 +0200

    Updated loader.php

--- includes/loader.php
+++ includes/loader.php
@@ -6,16 +6,17 @@
 /** Define PHP_VERSION_ID if not defined */
 if(!defined('PHP_VERSION_ID')) {
 	$phpVersion = explode('.', PHP_VERSION);
-	define('PHP_VERSION_ID', $version[0] * 10000 + $version[1] * 100 + $version[2]);
+	define('PHP_VERSION_ID', $phpVersion[0] * 10000 + $phpVersion[1] * 100 + $phpVersion[2]);
 }
 
 /** Include Oli files */
-foreach(array_merge(glob(INCLUDEPATH . '*.php'), glob(INCLUDEPATH . '*.php')) as $filename) {
-    require_once $filename;
+require_once INCLUDEPATH . 'OliCore.php'; // Oli core file
+foreach(array_merge(glob(INCLUDEPATH . '*.php'), glob(INCLUDEPATH . '*/*.php')) as $filename) {
+	if($filename != INCLUDEPATH . 'OliCore.php') require_once $filename;
 }
 
 /** Load Addons files */
 foreach(array_merge(glob(ADDONSPATH . '*.php'), glob(ADDONSPATH . '*/*.php')) as $filename) {
-    include_once $filename;
+	include_once $filename;
 }
 ?>
