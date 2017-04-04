8a63b03ce1860fe79710765c6940c2e7b2d446cb
Matiboux <matiboux@gmail.com>
2017-04-04 21:27:07 +0200

    Updated PHP-Addons file header

--- includes/PHP-Addons.php
+++ includes/PHP-Addons.php
@@ -1,25 +1,20 @@
 <?php
-/**
- * PHP Addons for Oli
- * 
- * Adds more functions to native PHP ones
- * Please check OliCore.php file for more info on this framework
- * 
- * @see OliFramework/OliCore for more info on this framework
- * @copyright 2015 Matiboux
- */
+/*\
+|*|  ------------------------
+|*|  --- [  PHP Addons  ] ---
+|*|  --- [   for Oli    ] ---
+|*|  ------------------------
+|*|  
+|*|  PHP Addons is a build-in addon for Oli.
+|*|  It is meant to add more general functions to the native PHP ones.
+|*|  
+|*|  For more info on Oli, please see the its Github repository: https://github.com/OliFramework/Oli/
+\*/
 
 namespace {
 
-/**
-  * Check if an array is associative or not
-  * 
-  * @param array $array Array to check
-  * 
-  * @since 1.7.0
-  * @see http://stackoverflow.com/a/173479/5255556 Answer where this function comes from
-  * @return bool
-  */
+/** Is Array Associative? */
+// Inspirated by http://stackoverflow.com/a/173479/5255556
 function is_assoc($array) {
 	return is_array($array) ? !empty($array) AND array_keys($array) !== range(0, count($array) - 1) : false;
 }
