99a23fcc1206b9d59d1b6b746ebfe5cd3426519f
Matiboux <matiboux@gmail.com>
2017-07-07 01:08:18 +0200

    Updated OliCore::verifyAuthKey() and OliCore::getAuthKeyOwner() for API use

--- includes/OliCore.php
+++ includes/OliCore.php
@@ -2737,16 +2737,16 @@ class OliCore {
 			public function isEmptyAuthKey() { return $this->isEmptyCookie($this->config['auth_key_cookie']['name']); }
 			
 			/** Verify Auth Key validity */
-			public function verifyAuthKey($authKey = null) {
+			public function verifyAuthKey($authKey = null, $userID = null) {
 				if(!$this->config['user_management']) trigger_error('Sorry, the user management has been disabled.', E_USER_ERROR);
-				else if(!empty($authKey = hash('sha512', $authKey ?: $this->getAuthKey())) AND ($sessionInfos = $this->getAccountLines('SESSIONS', array('user_id' => $this->cache['userID'])))['auth_key'] == $authKey AND $sessionInfos['ip_address'] == $this->getUserIP() AND strtotime($sessionInfos['expire_date']) >= time()) return true;
+				else if(!empty($authKey = hash('sha512', $authKey ?: $this->getAuthKey())) AND ($sessionInfos = $this->getAccountLines('SESSIONS', array('user_id' => $userID ?: $this->cache['userID'])))['auth_key'] == $authKey AND $sessionInfos['ip_address'] == $this->getUserIP() AND strtotime($sessionInfos['expire_date']) >= time()) return true;
 				else return false;
 			}
 			
 			/** Get Auth Key Owner */
-			public function getAuthKeyOwner($authKey = null) {
+			public function getAuthKeyOwner($authKey = null, $userID = null) {
 				if(!$this->config['user_management']) trigger_error('Sorry, the user management has been disabled.', E_USER_ERROR);
-				else if($this->verifyAuthKey($authKey = $authKey ?: $this->getAuthKey())) return $this->getAccountInfos('SESSIONS', 'username', array('auth_key' => hash('sha512', $authKey)));
+				else if($this->verifyAuthKey($authKey = $authKey ?: $this->getAuthKey(), $userID ?: $this->cache['userID'])) return $this->getAccountInfos('SESSIONS', 'username', array('auth_key' => hash('sha512', $authKey)));
 				else return false;
 			}
 		
