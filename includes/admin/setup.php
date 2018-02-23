5840648285bad74374187381920534535d6e239d
Matiboux <matiboux@gmail.com>
2018-02-23 23:16:50 +0100

    Updated step progression system in admin script setup.php
    Updated JS script
    Added explicit script selector

--- includes/admin/setup.php
+++ includes/admin/setup.php
@@ -2,6 +2,9 @@
 $params = array_merge($_GET, $_POST);
 $result = [];
 
+if(!empty($params['formdata'])) $formdata = json_decode(base64_decode($params['formdata']), true);
+else $formdata = null;
+
 if(!empty($params)) {
 	if($params['action'] == 'setLoginInfos') {
 		if(!$_Oli->config['user_management']) $result = array('error' => 'Error: User Management is disabled');
@@ -55,8 +58,13 @@ $result = json_encode(!empty($result) ? $result : array('error' => 'Unknown scri
 <h1>Oli Initial Setup —</h1>
 <p>Looks like your website lacks basic config. Please follow the instructions to setup your website.</p>
 
+Step <button type="button" onclick="nextStep(1);">1</button>
+<button type="button" onclick="nextStep(2);">2</button>
+<button type="button" onclick="nextStep(3);">3</button>
+<button type="button" onclick="nextStep(4);">4</button>
+
 <form action="#" method="post">
-	<div step="1">
+	<div class="step" step="1">
 		<h2>Step 1/4 — Verify your identity</h2>
 		<?php if(!file_get_contents(ABSPATH . '.olisc') OR time() > filemtime(ABSPATH . '.olisc') + 3600*2 OR empty(file_get_contents(ABSPATH . '.olisc'))) {
 			$handle = fopen(ABSPATH . '.olisc', 'w');
@@ -68,11 +76,11 @@ $result = json_encode(!empty($result) ? $result : array('error' => 'Unknown scri
 		<?php } ?>
 		
 		<p>In order to verify that you are the owner of this website, please type in below the generated security code. You can find the file containing the security code in the main folder of your website.</p>
-		<input type="text" name="olisc" placeholder="Oli Security Code" />
-		<button type="button" onclick="nextStep(this.parentNode.getAttribute('step'));">Verify my identity</button>
+		<input type="text" name="olisc" placeholder="Oli Security Code" value="<?=$_POST['olisc']?>" />
+		<button type="button" onclick="nextStep();">Verify my identity</button>
 	</div>
 		
-	<div step="2" style="display: none">
+	<div class="step" step="2" style="display: none">
 		<h2>Step 2/4 — Define the base url</h2>
 		<?php $baseurl = $_Oli->getUrlParam(0); ?>
 		<p>To ensure that your website works properly, please select which one of these addresses should be the base url (or home page) of your website.</p>
@@ -84,20 +92,20 @@ $result = json_encode(!empty($result) ? $result : array('error' => 'Unknown scri
 				<option value="<?=implode(array_slice(explode('://', $baseurl), 1))?>"><?=$baseurl?></option>
 			<?php } ?>
 		</select>
-		<button type="button" onclick="nextStep(this.parentNode.getAttribute('step'));">Confirm this address</button>
+		<button type="button" onclick="nextStep();">Confirm this address</button>
 	</div>
 		
-	<div step="3" style="display: none">
+	<div class="step" step="3" style="display: none">
 		<h2>Step 3/4 — Your website basic infos</h2>
 		<p>Finally, let's add some basic information that makes the identity of your website.</p>
-		Website name: <input type="text" name="name" placeholder="Name" /> <br />
-		Website description: <input type="text" name="description" placeholder="Description" /> <br />
-		Website creation date: <input type="date" name="creation_date" placeholder="Creation date" /> <br />
-		Website owner: <input type="text" name="owner" placeholder="Owner" /> <br />
-		<button type="button" onclick="nextStep(this.parentNode.getAttribute('step'));">Confirm those infos</button>
+		Website name: <input type="text" name="name" placeholder="Name" value="<?=$_POST['name']?>" /> <br />
+		Website description: <input type="text" name="description" placeholder="Description" value="<?=$_POST['description']?>" /> <br />
+		Website creation date: <input type="date" name="creation_date" placeholder="Creation date" value="<?=$_POST['creation_date']?>" /> <br />
+		Website owner: <input type="text" name="owner" placeholder="Owner" value="<?=$_POST['owner']?>" /> <br />
+		<button type="button" onclick="nextStep();">Confirm those infos</button>
 	</div>
 		
-	<div step="4" style="display: none">
+	<div class="step" step="4" style="display: none">
 		<h2>Step 4/4 — Thank you! ♥</h2>
 		<p>Huge thanks for using my framework! Please consider supporting my work and checking out my other projects.</p>
 		<p>You can also become a contributor of the project! Check out <a href="https://github.com/OliFramework/Oli/">Oli's Github repository</a>.</p>
@@ -110,15 +118,27 @@ $result = json_encode(!empty($result) ? $result : array('error' => 'Unknown scri
 
 <script>
 // TODO: Disable submit by enter
-var nextStep = function(step) {
-	if(step == 1 && document.querySelector('[name="olisc"]').value == '') alert('Step 1 error');
-	else if(step == 2 && document.querySelector('[name="baseurl"]').value == '') alert('Step 2 error');
-	else if(step == 3 && (document.querySelector('[name="name"]').value == '' || document.querySelector('[name="description"]').value == '' || document.querySelector('[name="creation_date"]').value == '' || document.querySelector('[name="owner"]').value == '')) alert('Step 3 error');
-	else {
-		var nextStepElem = document.querySelector('[step="' + (Number(step)+1) + '"]')
+var step = 1;
+var nextStep = function(next) {
+	var error = null;
+	next = next || step+1;
+	
+	if(next <= 0 || next > 4) return false;
+	else if(next > 1 && document.querySelector('[name="olisc"]').value == '') error = 'Step 1 error';
+	else if(next > 2 && document.querySelector('[name="baseurl"]').value == '') error = 'Step 2 error';
+	else if(next > 3 && (document.querySelector('[name="name"]').value == '' || document.querySelector('[name="description"]').value == '' || document.querySelector('[name="creation_date"]').value == '' || document.querySelector('[name="owner"]').value == '')) error = 'Step 3 error';
+	
+	if(error) {
+		alert(error);
+		return false;
+	} else {
+		var nextStepElem = document.querySelector('[step="' + next + '"]')
 		if(nextStepElem !== null) {
-			document.querySelector('[step="' + step + '"]').style.display = "none";
-			document.querySelector('[step="' + (Number(step)+1) + '"]').style.display = "block";
+			for(elem of document.querySelectorAll('.step')) {
+				elem.style.display = "none";
+			}
+			document.querySelector('[step="' + next + '"]').style.display = "block";
+			step = next;
 		} else alert('An error occurred!');
 	}
 }
