f87f777ebf8f232a223719d82d773a452eacde0b
Matiboux <matiboux@gmail.com>
2018-03-02 01:51:37 +0100

    Updated admin script index.php
    #Inappropriate..

--- includes/admin/index.php
+++ includes/admin/index.php
@@ -1,4 +1,23 @@
-OLI ADMIN PANEL \o/
+<?php
+$params = array_merge($_GET, $_POST);
+$result = [];
 
-- Login
-- AHHHHHHHHHH
+?>
+
+<html>
+<head>
+
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="author" content="Matiboux" />
+
+<title>Oli Admin</title>
+
+</head>
+<body>
+
+<p><u><b>Go login you fuck boy</b></u></p>
+<p><i>Oh shit, inappropriate..</i></p>
+
+</body>
+</html>
