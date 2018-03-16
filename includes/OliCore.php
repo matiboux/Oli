ffbff95925e8f92e30c5fd23ddd867d6c98d2d4b
Matiboux <matiboux@gmail.com>
2018-03-17 00:18:44 +0100

    Updated OliCore::setupMySQL() to use the new "allow_mysql" config
    Now setup a PDO MySQL connection only if "allow_mysql" config is true.

--- includes/OliCore.php
+++ includes/OliCore.php
@@ -528,7 +528,7 @@ class OliCore {
 		 * @return boolean Returns true if succeeded.
 		 */
 		public function setupMySQL($database, $username = 'root', $password = '', $hostname = 'localhost', $charset = 'utf-8') {
-			if(!empty($database)) {
+			if($this-config['allow_mysql'] AND !empty($database)) {
 				try {
 					$this->db = new \PDO('mysql:dbname=' . $database . ';host=' . $hostname . ';charset=' . $charset, $username, $password);
 					$this->mysqlConfig = array('database' => $database, 'username' => $username, 'password' => $password, 'hostname' => $hostname, 'charset' => $charset);
