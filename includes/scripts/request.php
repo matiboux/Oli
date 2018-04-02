66e17307ecf6b574cd703f18d4c89d34ca4468ea
Matiboux <matiboux@gmail.com>
2018-04-03 01:27:00 +0200

    Updated request.php script

--- includes/scripts/request.php
+++ includes/scripts/request.php
@@ -1,20 +1,20 @@
 <?php
-$params = array_merge($_GET, $_POST);
+$_ = array_merge($_GET, $_POST);
 $result = [];
 
-if(!empty($params)) {
-	if($params['action'] == 'setLoginInfos') {
+if(!empty($_)) {
+	if($_['action'] == 'setLoginInfos') {
 		if(!$_Oli->config['user_management']) $result = array('error' => 'Error: User Management is disabled');
-		else if(empty($params['authKey'])) $result = array('error' => 'Error: "authKey" parameter is missing');
-		else if(empty($params['userID'])) $result = array('error' => 'Error: "userID" parameter is missing');
+		else if(empty($_['authKey'])) $result = array('error' => 'Error: "authKey" parameter is missing');
+		else if(empty($_['userID'])) $result = array('error' => 'Error: "userID" parameter is missing');
 		else if($_Oli->verifyAuthKey()) $result = array('error' => 'Error: Cannot overwrite a valid authKey');
 		else {
-			$_Oli->setUserIDCookie($params['userID'], null);
-			$_Oli->setAuthKeyCookie($params['authKey'], $expireDelay = $params['extendedDelay'] ? $_Oli->config['extended_session_duration'] : $_Oli->config['default_session_duration']);
+			$_Oli->setUserIDCookie($_['userID'], null);
+			$_Oli->setAuthKeyCookie($_['authKey'], $expireDelay = $_['extendedDelay'] ? $_Oli->config['extended_session_duration'] : $_Oli->config['default_session_duration']);
 			
-			$result = array('error' => false, 'authKey' => $params['authKey'], 'userID' => $params['userID'], 'expireDelay' => $expireDelay);
+			$result = array('error' => false, 'authKey' => $_['authKey'], 'userID' => $_['userID'], 'expireDelay' => $expireDelay);
 		}
-	} else if($params['action'] == 'removeLoginInfos') {
+	} else if($_['action'] == 'removeLoginInfos') {
 		if(!$_Oli->config['user_management']) $result = array('error' => 'Error: User Management is disabled');
 		
 		if($_Oli->verifyAuthKey()) $result = array('error' => 'Error: Cannot remove valid login infos');
@@ -25,13 +25,13 @@ if(!empty($params)) {
 	} else $wrongAction = true;
 	
 	if($wrongAction) $result = array('error' => 'Error: "Action" parameter is missing');
-	else if(!empty($params['next'])) {
-		// $params['next'] = json_decode($params['next'], true);
-		$next = array_shift($params['next']);
-		// $params['next'] = !empty($params['next']) ? json_encode($params['next'], JSON_FORCE_OBJECT) : null;
+	else if(!empty($_['next'])) {
+		// $_['next'] = json_decode($_['next'], true);
+		$next = array_shift($_['next']);
+		// $_['next'] = !empty($_['next']) ? json_encode($_['next'], JSON_FORCE_OBJECT) : null;
 		
-		header('Location: ' . (substr($next, -1) == '/' ? $next : $next . '/') . 'request.php' . '?' . http_build_query($params));
-	} else if(!empty($params['callback'])) header('Location: ' . $params['callback']);
+		header('Location: ' . (substr($next, -1) == '/' ? $next : $next . '/') . 'request.php' . '?' . http_build_query($_));
+	} else if(!empty($_['callback'])) header('Location: ' . $_['callback']);
 } else $result = array('error' => 'Error: No parameters provided');
 
 die(!empty($result) ? json_encode($result, JSON_FORCE_OBJECT) : array('error' => 'Unknown script result.'));
