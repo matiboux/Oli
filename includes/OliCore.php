376623e3e21cd636f88102f62d715d7e76488677
Matiboux <matiboux@gmail.com>
2018-05-21 20:52:32 +0200

    Updated OliCore::uuid4() function
    Actually another comment proposed a more cryptography secure solution:
    Using random_bytes() over mt_rand().
    
    From https://stackoverflow.com/a/15875555/5255556
    
    Note: random_bytes() was added in PHP 7.
    For backward compatibility, openssl_random_pseudo_bytes() is used
    instead when random_bytes() isn't available.

--- includes/OliCore.php
+++ includes/OliCore.php
@@ -2738,32 +2738,19 @@ class OliCore {
 			/**
 			 * UUID v4 Generator Script
 			 * 
-			 * From Andrew Moore - http://www.php.net/manual/en/function.uniqid.php#94959
+			 * From https://stackoverflow.com/a/15875555/5255556
 			 * 
 			 * @version BETA-2.0.0
 			 * @updated BETA-2.0.0
 			 * @return string Returns the generated UUID.
 			 */
 			public function uuid4() {
-				return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-					// 32 bits for "time_low"
-					mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+				if(function_exists('random_bytes')) $data = random_bytes(16);
+				else $data = openssl_random_pseudo_bytes(16);
 
-					// 16 bits for "time_mid"
-					mt_rand( 0, 0xffff ),
-
-					// 16 bits for "time_hi_and_version",
-					// four most significant bits holds version number 4
-					mt_rand( 0, 0x0fff ) | 0x4000,
-
-					// 16 bits, 8 bits for "clk_seq_hi_res",
-					// 8 bits for "clk_seq_low",
-					// two most significant bits holds zero and one for variant DCE1.1
-					mt_rand( 0, 0x3fff ) | 0x8000,
-
-					// 48 bits for "node"
-					mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-				);
+				$data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+				$data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+				return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 			}
 			
 			/** ----------------------- */
