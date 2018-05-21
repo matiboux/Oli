39e34d3143b87ee0769c50051773e782506cec24
Matiboux <matiboux@gmail.com>
2018-05-21 20:29:00 +0200

    Added new OliCore::uuid4() function
    From Andrew Moore - http://www.php.net/manual/en/function.uniqid.php#94959
    (and from this answer on StackOverflow - https://stackoverflow.com/a/2040279/5255556)

--- includes/OliCore.php
+++ includes/OliCore.php
@@ -2735,6 +2735,37 @@ class OliCore {
 				}
 			}
 			
+			/**
+			 * UUID v4 Generator Script
+			 * 
+			 * From Andrew Moore - http://www.php.net/manual/en/function.uniqid.php#94959
+			 * 
+			 * @version BETA-2.0.0
+			 * @updated BETA-2.0.0
+			 * @return string Returns the generated UUID.
+			 */
+			public function uuid4() {
+				return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+					// 32 bits for "time_low"
+					mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+					// 16 bits for "time_mid"
+					mt_rand( 0, 0xffff ),
+
+					// 16 bits for "time_hi_and_version",
+					// four most significant bits holds version number 4
+					mt_rand( 0, 0x0fff ) | 0x4000,
+
+					// 16 bits, 8 bits for "clk_seq_hi_res",
+					// 8 bits for "clk_seq_low",
+					// two most significant bits holds zero and one for variant DCE1.1
+					mt_rand( 0, 0x3fff ) | 0x8000,
+
+					// 48 bits for "node"
+					mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+				);
+			}
+			
 			/** ----------------------- */
 			/**  VI. 9. C. Date & Time  */
 			/** ----------------------- */
