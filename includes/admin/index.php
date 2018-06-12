6588cf3c186f85f11dab494f42ef31fad7a843a5
Matiboux <matiboux@gmail.com>
2018-06-12 09:52:18 +0200

    Added Oli Admin Stylesheet

--- includes/admin/index.php
+++ includes/admin/index.php
@@ -13,12 +13,15 @@ if(!$_Oli->isLoggedIn()) header('Location: ' . $_Oli->getLoginUrl());
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="author" content="Matiboux" />
 
+<link rel="stylesheet" type="text/css" href="<?=$_Oli->getAdminAssetsUrl()?>style.css" />
+
 <title>Oli Admin</title>
 
 </head>
 <body>
 
-<h1>Oli Admin —</h1>
+<div id="header"><h1>Oli Admin — Home</h1></div>
+<div id="main">
 <p>Welcome on the Oli Admin, <?=$_Oli->getLoggedUsername()?>!</p>
 
 <ul>
@@ -26,6 +29,8 @@ if(!$_Oli->isLoggedIn()) header('Location: ' . $_Oli->getLoginUrl());
 	<li><a href="<?=$_Oli->getUrlParam(0) . $_Oli->getUrlParam(1) . '/config'?>">Edit your website config</a></li>
 	<li><a href="<?=$_Oli->getUrlParam(0) . $_Oli->getUrlParam(1) . '/mysql'?>">Edit your mysql config</a></li>
 </ul>
+
+</div>
 
 </body>
 </html>
