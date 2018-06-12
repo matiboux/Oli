3599ee1dd72384a557d892d935f4d2d85e977231
Matiboux <matiboux@gmail.com>
2018-06-12 23:55:10 +0200

    Updated Oli Admin pages menu

--- includes/admin/mysql.php
+++ includes/admin/mysql.php
@@ -62,8 +62,8 @@ else if($_Oli->getUrlParam(2) == 'basics') {
 
 <div id="navbar">
 	<ul class="navbar-nav float-left">
-		<li><a href="<?=$_Oli->getUrlParam(0)?>"><i class="fa fa-globe"></i></a></li>
-		<li><a href="<?=$_Oli->getAdminUrl()?>"><i class="fa fa-home"></i></a></li>
+		<li><a href="<?=$_Oli->getUrlParam(0)?>"><i class="fa fa-globe fa-fw"></i> <span class="d-none d-sm-inline"><?=explode('/', $_Oli->getUrlParam(0), 3)[2]?></span><span class="d-sm-none">Website</span></a></li>
+		<li><a href="<?=$_Oli->getAdminUrl()?>"><i class="fa fa-home fa-fw"></i> Oli Admin</a></li>
 	</ul>
 	<ul class="navbar-nav float-right">
 		<?php if($_Oli->isLoggedIn()) { ?>
