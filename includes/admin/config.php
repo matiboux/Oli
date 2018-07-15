2462aa698322465cf1678b22f4a3574a4478ce2b
Matiboux <matiboux@gmail.com>
2018-07-16 00:50:25 +0200

    Renamed admin page config2.php to config.php
    + Renamed old config.php page to config.old.php

--- includes/admin/config.php
+++ includes/admin/config.php
@@ -5,16 +5,31 @@ $result = [];
 if(!$_Oli->isLoggedIn()) header('Location: ' . $_Oli->getLoginUrl());
 else if($_Oli->getUserRightLevel() < $_Oli->translateUserRight('ROOT')) header('Location: ' . $_Oli->getAdminUrl());
 
-else if(!empty($_)) {
-	if(empty($_['database'])) $result = array('error' => 'Error: The database is missing.');
-	else {
-		$status = $_Oli->updateConfig(array('mysql' => array('database' => $_['database'], 'username' => isset($_['username']) ? $_['username'] : null, 'password' => isset($_['password']) ? $_['password'] : null, 'hostname' => isset($_['hostname']) ? $_['hostname'] : null, 'charset' => isset($_['charset']) ? $_['charset'] : null)), true, false);
-		
-		if($status) $result = array('error' => false, 'database' => $_['database']);
-		else $result = array('error' => 'Error: An error occurred.');
+/** Create Config */
+// .../create-config/ $level / $config
+else if($_Oli->getUrlParam(2) == 'create-config') {
+	
+
+/** Delete Config */
+// .../delete-config/ $level / $config
+} else if($_Oli->getUrlParam(2) == 'delete-config') {
+	
+
+/** With Form Data */
+} else if(!empty($_)) {
+	/** Add Config */
+	// .../add-config/
+	// POST ['config', 'global', 'local']
+	if($_Oli->getUrlParam(2) == 'add-config') {
+		// if(empty($_['database'])) $result = array('error' => 'Error: The database is missing.');
+		// else {
+			// $status = $_Oli->updateConfig(array('mysql' => array('database' => $_['database'], 'username' => isset($_['username']) ? $_['username'] : null, 'password' => isset($_['password']) ? $_['password'] : null, 'hostname' => isset($_['hostname']) ? $_['hostname'] : null, 'charset' => isset($_['charset']) ? $_['charset'] : null)), true, false);
+			
+			// if($status) $result = array('error' => false, 'database' => $_['database']);
+			// else $result = array('error' => 'Error: An error occurred.');
+		// }
 	}
 }
-
 ?>
 
 <html>
@@ -35,72 +50,173 @@ else if(!empty($_)) {
 
 <div id="main">
 	<?php if(!empty($result)) { ?><p><u><b>Script logs</b>:</u> <code><?=json_encode($result, JSON_FORCE_OBJECT)?></code></p><?php } ?>
-
-	<form action="#" method="post" id="form">
-		<h2>Love <3</h2>
-		
-		<p>Work in progress i guess.</p>
-		
-		<?php
-		function parseConfig($config, $tree = []) {
-			foreach($config as $eachVar => $eachValue) { ?>
+	
+	<?php /*<form action="#" method="post" id="form">*/ ?>
+	<h2>Your Website Configuration</h2>
+	<p>Work in progress!</p>
+	
+	<?php /*<button type="submit">Submit</button>*/ ?>
+	
+	<?php $config = [];
+	$defaultConfig = $_Oli->getDefaultConfig();
+	$globalConfig = $_Oli->getGlobalConfig();
+	$localConfig = $_Oli->getLocalConfig();
+	if(is_array($defaultConfig)) {
+		foreach ($defaultConfig as $key => $value) {
+			$config[$key]['default'] = $value;
+		}
+	}
+	if(is_array($globalConfig)) {
+		foreach($globalConfig as $key => $value) {
+			$config[$key]['global'] = $value;
+		}
+	}
+	if(is_array($localConfig)) {
+		foreach($localConfig as $key => $value) {
+			$config[$key]['local'] = $value;
+		}
+	} ?>
+	
+	<?php if($globalConfig === null) { ?>
+		<p>\o/ Global Config does not exist</p>
+	<?php } ?>
+	
+	<?php if($localConfig === null) { ?>
+		<p>\o/ Local Config does not exist</p>
+	<?php } ?>
+	
+	<style>
+	.btn { display: block }
+	</style>
+	
+	<table>
+		<thead>
+			<tr>
+				<th>Config</th>
+				<th>Default</th>
+				<th>Global</th>
+				<th>Local</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($config as $eachConfig => $eachValue) { ?>
 				<?php
-				if(in_array($tree[0] ?: $eachVar, ['init_timestamp', 'allow_mysql', 'settings'])) $disabled = true;
-				else $disabled = false;
-				if(in_array($tree[0] ?: $eachVar, ['mysql'])) $hidden = true;
-				else $hidden = false;
-				
-				if(!isset($eachValue)) $type = 'null';
-				else if(is_assoc($eachValue)) $type = 'assoc';
-				else if(is_array($eachValue)) $type = 'array';
-				else if(is_bool($eachValue)) $type = 'checkbox';
-				else if(is_integer($eachValue) OR is_float($eachValue)) $type = 'number';
-				else $type = 'text';
-				
-				$name = !empty($tree) ? $tree[0] . '[' . (count($tree) > 1 ? implode('][', array_slice($tree, 1)) . '][' : '') . $eachVar . ']' : $eachVar;
+				$type = [];
+				foreach($eachValue as $config => $value) {
+					if(!isset($value)) $type[$config] = 'null';
+					else if(is_assoc($value)) $type[$config] = 'assoc';
+					else if(is_array($value)) $type[$config] = 'array';
+					else if(is_bool($value)) $type[$config] = 'checkbox';
+					else if(is_integer($value) OR is_float($eachValue)) $type[$config] = 'number';
+					else $type[$config] = 'text';
+				}
 				?>
 				
-				<div class="config">
-					<span><b><?=$eachVar?></b> —</span>
-					<div class="single" style="display: <?php if(in_array($type, ['number', 'text', 'checkbox'])) { ?>inline-block<?php } else { ?>none<?php } ?>">
-						<label><input type="<?=$type?>" name="<?=$name?>" value="<?=!$hidden ? ($_[$eachVar] ?: $eachValue) : '[hidden]'?>" <?php if($disabled OR $hidden) { ?>disabled<?php } ?> /> <?php if($type == 'checkbox') { ?>Yes/No<?php } ?></label> —
-					</div>
-					<div class="settings" style="display: inline-block">
-						<label><input type="radio" class="type" name="type[<?=$name?>]" value="null" <?php if($type == 'null') { ?>checked<?php } ?> /> NULL</label>
-						<label><input type="radio" class="type" name="type[<?=$name?>]" value="number" <?php if($type == 'number') { ?>checked<?php } ?> /> Number</label>
-						<label><input type="radio" class="type" name="type[<?=$name?>]" value="text" <?php if($type == 'text') { ?>checked<?php } ?> /> Text</label>
-						<label><input type="radio" class="type" name="type[<?=$name?>]" value="checkbox" <?php if($type == 'checkbox') { ?>checked<?php } ?> /> Boolean</label>
-						<label><input type="radio" class="type" name="type[<?=$name?>]" value="array" <?php if($type == 'array') { ?>checked<?php } ?> /> Indexed arrays</label>
-						<label><input type="radio" class="type" name="type[<?=$name?>]" value="assoc" <?php if($type == 'assoc') { ?>checked<?php } ?> /> Associative arrays</label>
-					</div>
-					<div class="multiple" style="display: <?php if(in_array($type, ['array', 'assoc'])) { ?>block<?php } else { ?>none<?php } ?>">
-						<?php /*foreach($_[$eachVar] ?: $eachValue as $eachBVar => $eachBValue) { ?>
-							<?php
-							if(is_integer($eachValue) OR is_float($eachValue)) $type = 'number';
-							else $type = 'text';
-							?>
-							
-							<div style="display: inline-block">
-								<label><input type="" name="input1" value="<?=$eachBVar?>" <?php if($disabled) { ?>disabled<?php } ?> /></label> =>
-								<label><input type="<?=$type?>" name="input1" value="<?=$eachBValue?>" <?php if($disabled) { ?>disabled<?php } ?> /></label>
+				<tr>
+					<td><?=$eachConfig?></td>
+					<td class="disabled">
+						<?php /*<div class="single" style="display: <?php if(in_array($type['default'], ['number', 'text', 'checkbox'])) { ?>inline-block<?php } else { ?>none<?php } ?>">
+							<label><input type="<?=$type['default']?>" name="default[<?=$eachConfig?>]" value="<?=!$hidden ? ($_[$eachVar]['default'] ?: $eachValue['default']) : '[hidden]'?>" <?php if($disabled OR $hidden) { ?>disabled<?php } ?> /> <?php if($type['default'] == 'checkbox') { ?>Yes/No<?php } ?></label> —
+						</div>
+						<div class="settings" style="background: #f050a0; display: inline-block">
+							<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="null" <?php if($type['default'] == 'null') { ?>checked<?php } ?> /> NULL</label>
+							<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="number" <?php if($type['default'] == 'number') { ?>checked<?php } ?> /> Number</label>
+							<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="text" <?php if($type['default'] == 'text') { ?>checked<?php } ?> /> Text</label>
+							<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="checkbox" <?php if($type['default'] == 'checkbox') { ?>checked<?php } ?> /> Boolean</label>
+							<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="array" <?php if($type['default'] == 'array') { ?>checked<?php } ?> /> Indexed arrays</label>
+							<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="assoc" <?php if($type['default'] == 'assoc') { ?>checked<?php } ?> /> Associative arrays</label>
+						</div>
+						<div class="multiple" style="display: <?php if(in_array($type['default'], ['array', 'assoc'])) { ?>block<?php } else { ?>none<?php } ?>">
+							<?=var_export($eachValue['default'])?>
+						</div>*/ ?>
+				
+						<pre><?=isset($eachValue['default']) ? var_export($eachValue['default']) : null?></pre>
+					</td>
+					<td class="<?php if($globalConfig === null) { ?>disabled<?php } else if(!isset($eachValue['global'])) { ?>empty<?php } ?>">
+						<?php if(!isset($type['global'])) { ?>
+							<a href="<?=$_Oli->getUrlParam(0) . $_Oli->getUrlParam(1) . '/create-config/global/' . urlencode($eachConfig) . '/'?> " class="btn">Create config</a>
+							<i>&laquo; Inherit from Default</i>
+						<?php } else if($globalConfig !== null) { ?>
+							<a href="<?=$_Oli->getUrlParam(0) . $_Oli->getUrlParam(1) . '/delete-config/global/' . urlencode($eachConfig) . '/'?> " class="btn">Delete config</a>
+							<div class="single" style="display: <?php if(in_array($type['global'], ['number', 'text', 'checkbox'])) { ?>inline-block<?php } else { ?>none<?php } ?>">
+								<label><input type="<?=$type['global']?>" name="default[<?=$eachConfig?>]" value="<?=!$hidden ? ($_[$eachVar]['global'] ?: $eachValue['global']) : '[hidden]'?>" <?php if($disabled OR $hidden) { ?>disabled<?php } ?> <?php if($type['global'] == 'checkbox' AND !$hidden AND ($_[$eachVar]['global'] ?: $eachValue['global'])) { ?>checked<?php } ?> /> <?php if($type['global'] == 'checkbox') { ?>Yes/No<?php } ?></label> —
 							</div>
-							<div class="settings" style="display: inline-block">
-								<label><input type="radio" class="type" name="type[<?=$eachVar?>][<?=$eachBVar?>]" <?php if($type == 'number') { ?>checked<?php } ?> /> Number</label>
-								<label><input type="radio" class="type" name="type[<?=$eachVar?>][<?=$eachBVar?>]" <?php if($type == 'text') { ?>checked<?php } ?> /> Text</label>
-							</div> <br />
-						<?php }*/ if(in_array($type, ['array', 'assoc'])) parseConfig($_[$eachVar] ?: $eachValue, array_merge($tree, [$eachVar])); ?>
-					</div>
-				</div>
-			<?php }
-		} parseConfig($_Oli->config); ?>
-		
-		<button type="submit">Submit</button>
-		
-	</form>
+							<div class="settings" style="background: #f050a0; display: inline-block">
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="null" <?php if($type['global'] == 'null') { ?>checked<?php } ?> /> NULL</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="number" <?php if($type['global'] == 'number') { ?>checked<?php } ?> /> Number</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="text" <?php if($type['global'] == 'text') { ?>checked<?php } ?> /> Text</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="checkbox" <?php if($type['global'] == 'checkbox') { ?>checked<?php } ?> /> Boolean</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="array" <?php if($type['global'] == 'array') { ?>checked<?php } ?> /> Indexed arrays</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="assoc" <?php if($type['global'] == 'assoc') { ?>checked<?php } ?> /> Associative arrays</label>
+							</div>
+							<div class="multiple" style="display: <?php if(in_array($type['global'], ['array', 'assoc'])) { ?>block<?php } else { ?>none<?php } ?>">
+								<pre><?=var_export($eachValue['global'])?></pre>
+							</div>
+						<?php } ?>
+						
+						<?=''//isset($eachValue['global']) ? var_export($eachValue['global']) : null?>
+					</td>
+					<td class="<?php if($localConfig === null) { ?>disabled<?php } else if(!isset($eachValue['local'])) { ?>empty<?php } ?>">
+						<?php if(!isset($type['local'])) { ?>
+							<a href="<?=$_Oli->getUrlParam(0) . $_Oli->getUrlParam(1) . '/create-config/local/' . urlencode($eachConfig) . '/'?> " class="btn">Create config</a>
+							<i>&laquo; Inherit from Global</i>
+						<?php } else if($localConfig !== null) { ?>
+							<a href="<?=$_Oli->getUrlParam(0) . $_Oli->getUrlParam(1) . '/delete-config/local/' . urlencode($eachConfig) . '/'?> " class="btn">Delete config</a>
+							<div class="single" style="display: <?php if(in_array($type['local'], ['number', 'text', 'checkbox'])) { ?>inline-block<?php } else { ?>none<?php } ?>">
+								<label><input type="<?=$type['local']?>" name="default[<?=$eachConfig?>]" value="<?=!$hidden ? ($_[$eachVar]['local'] ?: $eachValue['local']) : '[hidden]'?>" <?php if($disabled OR $hidden) { ?>disabled<?php } ?> <?php if($type['local'] == 'checkbox' AND !$hidden AND ($_[$eachVar]['local'] ?: $eachValue['local'])) { ?>checked<?php } ?> /> <?php if($type['local'] == 'checkbox') { ?>Yes/No<?php } ?></label> —
+							</div>
+							<div class="settings" style="background: #f050a0; display: inline-block">
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="null" <?php if($type['local'] == 'null') { ?>checked<?php } ?> /> NULL</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="number" <?php if($type['local'] == 'number') { ?>checked<?php } ?> /> Number</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="text" <?php if($type['local'] == 'text') { ?>checked<?php } ?> /> Text</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="checkbox" <?php if($type['local'] == 'checkbox') { ?>checked<?php } ?> /> Boolean</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="array" <?php if($type['local'] == 'array') { ?>checked<?php } ?> /> Indexed arrays</label>
+								<label><input type="radio" class="type" name="type[default][<?=$eachConfig?>]" value="assoc" <?php if($type['local'] == 'assoc') { ?>checked<?php } ?> /> Associative arrays</label>
+							</div>
+							<div class="multiple" style="display: <?php if(in_array($type['local'], ['array', 'assoc'])) { ?>block<?php } else { ?>none<?php } ?>">
+								<pre><?=var_export($eachValue['local'])?></pre>
+							</div>
+						<?php } ?>
+						
+						<?=''//isset($eachValue['local']) ? var_export($eachValue['local']) : null?>
+					</td>
+				</tr>
+			<?php } ?>
+			<tr style="background: rgba(160, 80, 240, .1)">
+				<form action="#" method="post" id="form">
+					<td>
+						(+)
+						<input type="text" name="config" /> <br />
+						Once all fields are completed, 
+						<button type="submit">Add config</button>
+					</td>
+					<td>Default config cannot be modified.</td>
+					<td>
+						<input type="text" name="global" placeholder="Leave blank for Inherit" />
+					</td>
+					<td>
+						<input type="text" name="local" placeholder="Leave blank for Inherit" />
+					</td>
+				</form>
+			</tr>
+		</tbody>
+		<tfoot>
+			<?php /*<tr>
+				<th></th>
+				<th></th>
+				<th></th>
+				<th></th>
+			</tr>*/ ?>
+		</tfoot>
+	</table>
+	
+	<?php /*<button type="submit">Submit</button>*/ ?>
+	
+	<?php /*</form>*/ ?>
 
 	<script>
 	// TODO: Disable submit by enter
-	var step = 1;
+	/*var step = 1;
 	var nextStep = function(next) {
 		var error = null;
 		var last = 4;
@@ -134,7 +250,7 @@ else if(!empty($_)) {
 				step = next;
 			} else alert('An error occurred!');
 		}
-	}
+	}*/
 	</script>
 </div>
 
