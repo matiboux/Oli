b8b24bbd5c075e900f79bd25cf55034dd8cd3356
Matiboux <matiboux@gmail.com>
2021-09-19 17:27:10 +0200

    Move Oli source files to includes/src/

--- includes/loader.php
+++ includes/loader.php
@@ -4,23 +4,23 @@
 // ------------ //
 
 // Load librairies
-require_once INCLUDESPATH . 'PHP-Addons.php';
-require_once INCLUDESPATH . 'ErrorManager.php';
+require_once INCLUDESPATH . 'src/PHP-Addons.php';
+require_once INCLUDESPATH . 'src/ErrorManager.php';
 
-require_once INCLUDESPATH . 'SQLWrapper.php';
+require_once INCLUDESPATH . 'src/SQLWrapper.php';
 
 // Load Config
-require_once INCLUDESPATH . 'Config.php'; // Oli Config Registry
+require_once INCLUDESPATH . 'src/Config.php'; // Oli Config Registry
 \Oli\Config::loadRawConfig();
 $_OliConfig = &\Oli\Config::$config; // Config array alias
 
 // Load Oli
-require_once INCLUDESPATH . 'OliCore.php'; // Oli Core
+require_once INCLUDESPATH . 'src/OliCore.php'; // Oli Core
 if(\Oli\Config::$rawConfig['oli_mode'] == 'lite') {
-	require_once INCLUDESPATH . 'OliLite.php'; // Oli Lite
+	require_once INCLUDESPATH . 'src/OliLite.php'; // Oli Lite
 	$_Oli = new \Oli\OliLite(INITTIME);
 } else {
-	require_once INCLUDESPATH . 'OliFramework.php'; // Oli Framework
+	require_once INCLUDESPATH . 'src/OliFramework.php'; // Oli Framework
 	$_Oli = new \Oli\OliFramework(INITTIME);
 }
 
