3925b12c89ba50fb3760db9da4775144c982c91f
Matiboux <matiboux@gmail.com>
2021-10-03 22:45:56 +0200

    Refactor request.php to use the Script class

--- includes/scripts/request.php
+++ includes/scripts/request.php
@@ -1,34 +1,54 @@
 <?php
-$result = [];
+use Oli\Script;
 
-if (!empty($_))
+$script = new Script();
+$scriptOrderArgs = [['authKey', 'expireDelay'], true];
+
+if (empty($_['action']))
+	$script->setErrorMessage('Parameter "action" is missing.')
+	       ->printJSON(...$scriptOrderArgs)->exit();
+
+if ($_['action'] === 'setUserID' || $_['action'] === 'setLoginInfos')
 {
-	if ($_['action'] == 'setUserID' or $_['action'] == 'setLoginInfos')
-	{
-		if (!$_Oli->isAccountsManagementReady()) $result = ['error' => 'Error: User Management is disabled'];
-		else if (empty($_['authKey'])) $result = ['error' => 'Error: "authKey" parameter is missing'];
-		// else if(empty($_['userID'])) $result = array('error' => 'Error: "userID" parameter is missing');
-		else if ($_Oli->isLoggedIn()) $result = ['error' => 'Error: Cannot overwrite a valid authKey'];
-		else
-		{
-			$_Oli->setAuthKeyCookie($_['authKey'], $_Oli->config['auth_key_cookie']['expire_delay'] ?: 3600 * 24 * 7);
-			$result = ['error' => false, 'authKey' => $_['authKey'], 'expireDelay' => $expireDelay];
-		}
-	}
-	else $result = ['error' => 'Error: "Action" parameter is missing'];
-	if (!empty($result)) echo json_encode($result);
+	/** @var \Oli\OliCore $_Oli */
+	/** @var \Oli\AccountsManager Accounts Manager */
+	$_AM = $_Oli->getAccountsManager();
 
-	if (!empty($_['next']))
-	{
-		// $_['next'] = json_decode($_['next'], true);
-		$next = array_shift($_['next']);
-		// $_['next'] = !empty($_['next']) ? json_encode($_['next']) : null;
+	if ($_AM === null || !$_AM->isReady())
+		$script->setErrorMessage('User Management is disabled.')
+			   ->printJSON(...$scriptOrderArgs)->exit();
 
-		header('Location: ' . (substr($next, -1) == '/' ? $next : $next . '/') . 'request.php' . '?' . http_build_query($_));
-	}
-	else if (!empty($_['callback'])) header('Location: ' . $_['callback']);
+	if (empty($_['authKey']))
+		$script->setErrorMessage('Parameter "authKey" is missing.')
+		       ->printJSON(...$scriptOrderArgs)->exit();
+
+	// if (empty($_['userID']))
+		// $script->setErrorMessage('Parameter "userID" is missing.')
+		//        ->printJSON(...$scriptOrderArgs)->exit();
+
+	if ($_AM->isLoggedIn())
+		$script->setErrorMessage('Cannot overwrite a valid authentication key.')
+		       ->printJSON(...$scriptOrderArgs)->exit();
+
+	$expireDelay = $_OliConfig['auth_key_cookie']['expire_delay'] ?: 3600 * 24 * 7;
+	$_AM->setAuthKeyCookie($_['authKey'], $expireDelay);
+	$script->set('authKey', $_['authKey']);
+	$script->set('expireDelay', $expireDelay);
 }
-else echo json_encode(['error' => 'Error: No parameters provided']);
 
-// die(!empty($result) ? json_encode($result) : ['error' => 'Unknown script result.']);
-exit;
+else
+	$script->setErrorMessage('Parameter "action" is invalid.')
+	       ->printJSON(...$scriptOrderArgs)->exit();
+
+if (!empty($_['next']))
+{
+	// $_['next'] = json_decode($_['next'], true);
+	$next = array_shift($_['next']);
+	// $_['next'] = !empty($_['next']) ? json_encode($_['next']) : null;
+
+	header('Location: ' . (substr($next, -1) == '/' ? $next : $next . '/') . 'request.php' . '?' . http_build_query($_));
+}
+else if (!empty($_['callback']))
+	header('Location: ' . $_['callback']);
+
+$script->printJSON(...$scriptOrderArgs)->exit();
